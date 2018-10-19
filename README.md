41de97bd2a2d52e2d24f8364cf8ec63524822681
andynet <andrejbalaz001@gmail.com>
2018-10-19 21:09:18 +0200

    read_vcf finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# mutation_rates
+# Mutation rates
+
+## Project structure
+* data/
+    * test/
+* scripts/
+    * python/
+* workflow.py
+* README.md
---
+++ vcf_to_ped.py
@@ -0,0 +1,30 @@
+import argparse
+
+
+def read_vcf(file_name):
+
+    description = None
+    records = []
+
+    with open(file_name) as f:
+        for line in f:
+
+            if line[0:2] != '##':
+                if line[0] == '#':
+                    description = line.strip()
+                else:
+                    records.append(line.strip())
+
+    return description, records
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert vcf file to ped')
+    parser.add_argument('--vcf', required=True)
+    args = parser.parse_args()
+
+    description, records = read_vcf(args.vcf)
+
+
+if __name__ == '__main__':
+    main()
