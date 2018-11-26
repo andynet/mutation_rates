a3d2a1eef96fdebd5cab1930118b65fb8faaeeb9
andynet <andrejbalaz001@gmail.com>
2018-11-26 11:16:59 +0100

    added different directories

--- hapi_approach/workflow.py
+++ hapi_approach/workflow.py
@@ -15,13 +15,15 @@ def convert_vcf(vcf, prefix):
     return inputs, outputs, options, spec
 
 
-def run_hapi(hapi_exe, dat, _map, ped):
+def run_hapi(hapi_exe, dat, _map, ped, out_dir):
 
     inputs = [f'{dat}', f'{_map}', f'{ped}']
     outputs = [f'{ped}.finished']
     options = {}
     spec = f'''
-        {hapi_exe} -i {dat} {_map} {ped}
+        {hapi_exe} -i {dat} {_map} {ped}    \
+                   -d {out_dir}
+
         touch {ped}.finished
     '''
 
@@ -34,24 +36,34 @@ def main(workflow):
         config = yaml.safe_load(f)
 
     project_dir = config['project_dir']
-    input_vcf = config['input_vcf']
-    base = os.path.basename(input_vcf)
-    hapi_exe = config['hapi_dir']
+    chromosomes = config['chromosomes'].split(',')
+    generic_input_vcf = config['generic_input_vcf']
+    hapi_exe = config['hapi_exe']
+
     hapi_dir = f'{project_dir}/data_hapi_approach'
     os.makedirs(hapi_dir, mode=0o775, exist_ok=True)
 
-    tmp = base.replace('.vcf', '')
-    prefix = f'{hapi_dir}/{tmp}'
+    for _chr in chromosomes:
+        input_vcf = generic_input_vcf.replace('CHROMOSOME', _chr)
+        base = os.path.basename(input_vcf)
 
-    name = f'convert_vcf_{base}'
-    template = convert_vcf(input_vcf, prefix)
-    workflow.target_from_template(name, template)
+        chr_dir = f'{hapi_dir}/{_chr}'
+        os.makedirs(chr_dir, mode=0o775, exist_ok=True)
 
-    dat, _map, ped = template[1]
+        tmp = base.replace('.vcf', '')
+        prefix = f'{chr_dir}/{tmp}'
 
-    name = f'run_hapi_{base}'
-    template = run_hapi(hapi_exe, dat, _map, ped)
-    workflow.target_from_template(name, template)
+        name = f'convert_vcf_{base}'
+        template = convert_vcf(input_vcf, prefix)
+        print(template[3].strip())
+        workflow.target_from_template(name, template)
+
+        dat, _map, ped = template[1]
+
+        name = f'run_hapi_{base}'
+        template = run_hapi(hapi_exe, dat, _map, ped, chr_dir)
+        print(template[3].strip())
+        workflow.target_from_template(name, template)
 
 
 gwf = Workflow()
